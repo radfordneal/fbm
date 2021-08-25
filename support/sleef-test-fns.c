bc308437ad1a20f1573b66b0a89b0712f26abe52
Radford Neal <radfordneal@gmail.com>
2021-08-24 23:03:45 -0400

    update scalar sleef tests

--- support/sleef-test-fns.c
+++ support/sleef-test-fns.c
@@ -2,10 +2,10 @@
 #include <string.h>
 
 #include "../net/intrinsics-use.h"
-#include "../net/sleef-use-scalar.h"
+#include "sleef-use-scalar.h"
 
 double do_tanh  (double a) { return fast_tanh(a); }
-double do_tanhx (double a) { return 2/(1+fast_exp(-2*a)) - 1; }
+double do_tanhx (double a) { return 1 - 2/(1+fast_exp(a+a)); }
 double do_exp   (double a) { return fast_exp(a); }
 double do_log   (double a) { return fast_log(a); }
 double do_zero  (double a) { return 0.0; }
